945c3582dddd069f1f28e103f1cfc1777b2cc779
Agetedo <agetedo@gmail.com>
2024-10-14 20:15:21 +0300

    add <Intro />

---
+++ src/components/Intro.tsx
@@ -0,0 +1,11 @@
+import introImage from "/card-intro-image.png";
+import styles from "../styles/Intro.module.scss";
+
+export default function Intro() {
+  return (
+    <section className={styles.intro}>
+      <img className={styles.introImage} src={introImage} alt="#" />
+
+    </section>
+  )
+}
