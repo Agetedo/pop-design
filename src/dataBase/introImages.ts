6101d668294da34cfa30675a54a05183067b22f3
Agetedo <agetedo@gmail.com>
2024-10-16 21:39:35 +0300

    add scss Admin

--- src/dataBase/introImages.ts
+++ src/dataBase/introImages.ts
@@ -7,7 +7,6 @@ interface IntroImgType {
     
 export const introImages: IntroImgType[] = [{
   id: "iI1",
-<<<<<<< HEAD
   imageSrc: "/intro-image01.jpg",
   imgAlt: "",
   title: "All Your cards",
@@ -21,19 +20,4 @@ export const introImages: IntroImgType[] = [{
   imageSrc: "/intro-image03.jpg",
   imgAlt: "",
   title: "In one click",
-=======
-  imageSrc: "/card-intro-image.png",
-  imgAlt: "",
-  title: "11",
-}, {
-  id: "iI2",
-  imageSrc: "/card-intro-image.png",
-  imgAlt: "",
-  title: "22",
-}, {
-  id: "iI3",
-  imageSrc: "/card-intro-image.png",
-  imgAlt: "",
-  title: "33",
->>>>>>> origin/main
 }]
