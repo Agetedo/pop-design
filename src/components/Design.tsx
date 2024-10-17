c4186974d012b48749bce8b1b2a005aef80ead25
Agetedo <agetedo@gmail.com>
2024-10-18 01:18:24 +0300

    import Copyright

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -1,11 +1,16 @@
 import Conteiner from "./Conteiner";
 import DesignItems from "./DesignItems";
+import Copyright from "./Copyright";
 import styles from "../styles/Design.module.scss";
 
 export default function Design() {
   return (
     <Conteiner className={styles.design}>
       <DesignItems />
+      <Copyright 
+        copyright={"Copyright"}
+        text={"Pop Design Company"} 
+      />
     </Conteiner>
   )
 }
