a4943af1f5304c1f816adf4eb8ec53c6aef5e78c
Agetedo <agetedo@gmail.com>
2024-10-17 23:20:12 +0300

    add array data

---
+++ src/dataBase/buttons.ts
@@ -0,0 +1,23 @@
+interface ButtonsType {
+  id: string | number | bigint;
+  imgSrc: string;
+  buttonText: string;
+}
+  
+export const buttons: ButtonsType[] = [{
+  id: "bt1",
+  imgSrc: "",
+  buttonText: "Welcome",
+}, {
+  id: "bt2",
+  imgSrc: "",
+  buttonText: "Our Team",
+}, {
+  id: "bt3",
+  imgSrc: "",
+  buttonText: "Our Work",
+}, {
+  id: "bt4",
+  imgSrc: "",
+  buttonText: "Contact", 
+}];
