30e6a901ef9d390a13fb2c936e08270970412166
Agetedo <agetedo@gmail.com>
2024-10-17 23:20:33 +0300

    add import

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -1,18 +1,11 @@
 import Conteiner from "./Conteiner";
-<<<<<<< HEAD
 import DesignItems from "./DesignItems";
-=======
->>>>>>> origin/main
 import styles from "../styles/Design.module.scss";
 
 export default function Design() {
   return (
     <Conteiner className={styles.design}>
-<<<<<<< HEAD
       <DesignItems />
-=======
-      
->>>>>>> origin/main
     </Conteiner>
   )
 }
