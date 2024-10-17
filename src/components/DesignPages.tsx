635fa42f6edf75e1ee8044199f04bf7897379d74
Agetedo <agetedo@gmail.com>
2024-10-18 00:11:23 +0300

    {buttonList}

--- src/components/DesignPages.tsx
+++ src/components/DesignPages.tsx
@@ -1,12 +1,17 @@
+import { buttons } from "../dataBase/buttons";
 import styles from "../styles/DesignPages.module.scss";
 
 export default function DesignPages() {
+  const buttonsList = buttons.map( button =>
+    <button key={button.id} className={styles.pageButton}>
+      <img src={button.imgSrc} alt="#" />
+      <h2 className={styles.buttonText}>{button.buttonText}</h2>
+    </button>
+  )
+
   return(
     <div className={styles.designPages}>
-      <button className={styles.page}></button>
-      <button className={styles.page}></button>
-      <button className={styles.page}></button>
-      <button className={styles.page}></button>
+      {buttonsList}
     </div>
   );
 }
