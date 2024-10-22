feb139dce408868c813286386da069efad4024a2
Agetedo <agetedo@gmail.com>
2024-10-23 02:52:28 +0300

    interface TextContentProps

--- src/components/TeamContent.tsx
+++ src/components/TeamContent.tsx
@@ -1,16 +1,57 @@
+import teamImage from "/team.jpg";
 import styles from "../styles/TeamContent.module.scss";
+const teamText1 = <>{"When developing your custom solution, we ensure conformity with ISO 2700 and NIST 800 to make sure your web app is developed in line with benchmark security requirements. Our comprehensive InfoSec approach relies on multiple levels of protection."}</>;
+const teamText2 = <>{"When it comes to architecture design, we prepare your app for now and for the unexpected. That's why we create a robust, flexible server architecture."}</>;
+const teamText3 = <>{"We goes by your end user's needs, preferences, and behaviors to develop engaging web experiences"}</>;
 
-export default function TeamContent({ title  }: { title: string }) {
+export default function TeamContent({ title }: { title: string }) {
   return(
     <section className={styles.content}>
       <h2 className={styles.title}>{title}</h2>
-    
+
+      <div className={styles.contentItems}>
+        <TextContent 
+          text1={teamText1} 
+          text2={teamText2} 
+          text3={teamText3} 
+        />
+        <ImageContent imgAlt={"Image"} />
+      </div>
+      <ButtonLink buttonText={"Read More"} />
     </section>
   );
 } 
 
-/*interface Content1Props {
-  imgAlt1: string;
-  text1: string;
-  text2: string;
-}*/
+interface TextContentProps {
+  text1: string | typeof teamText1;
+  text2: string | typeof teamText2;
+  text3: string | typeof teamText3;
+}
+function TextContent({ text1, text2, text3 }: TextContentProps) {
+  return(
+    <div className={styles.textContent}>
+      <p className={styles.text}>{text1}</p>
+      <p className={styles.text}>{text2}</p>
+      <p className={styles.text}>{text3}</p>
+    </div>
+  );
+}
+
+function ImageContent({ imgAlt }: { imgAlt: string }) {
+  return(
+    <div className={styles.item}>
+      <img src={teamImage} alt={imgAlt} />
+    </div>
+  );
+}
+
+function ButtonLink({ buttonText }: { buttonText: string }) {
+  return(
+    <a href="https://developer.mozilla.org" 
+      rel="noopener" 
+      target="_blank"
+      className={styles.buttonLink}>
+      {buttonText}
+    </a>
+  );
+}
