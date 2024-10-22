1163d0098cd41ee8964717797030bf1aee7b9306
Agetedo <agetedo@gmail.com>
2024-10-22 22:37:53 +0300

    interface ButtonProps

---
+++ src/components/ButtonPage.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/ButtonPage.module.scss";
+
+interface ButtonProps {
+  imgSrc: string;
+  buttonText: string;
+  onClick: () => void;
+}
+
+export default function ButtonPage({ onClick, imgSrc, buttonText }: ButtonProps) {
+  return(
+    <button className={styles.pageButton} onClick={onClick}>
+      <img src={imgSrc} alt="#" />
+      <h2 className={styles.buttonText}>{buttonText}</h2>
+    </button>
+  );
+}
