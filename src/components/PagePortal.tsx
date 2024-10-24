ba886f4a91d37286f5d2941c5958abd9d6751de6
Agetedo <agetedo@gmail.com>
2024-10-25 01:46:18 +0300

    created <PagePortal />

---
+++ src/components/PagePortal.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import Wrapper from "./Wrapper";
+import closeIcon from "/modal-close-icon.png";
+import ButtonPage from "./ButtonPage";
+import { buttons } from "../dataBase/buttons";
+import styles from "../styles/PagePortal.module.scss";
+
+interface PagePortalProps {
+  button: string;
+  children?: React.ReactNode;
+};
+
+export default function PagePortal({ button, children }: PagePortalProps) {
+  const page = buttons.filter(modal =>
+    modal.buttonText === button
+  );
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      {page.map(modal =>
+        <ButtonPage key={modal.id} 
+          imgSrc={modal.imgSrc} 
+          buttonText={modal.buttonText} 
+          onClick={() => setShowModal(true)} 
+        />
+      )}
+      {showModal && createPortal(
+        <div className={styles.page}>
+          <Wrapper className={styles.pageWrapper}>
+            <div className={styles.pageContent}>
+              {children}
+              <button className={styles.modalClose} onClick={() => setShowModal(false)}>
+                <img src={closeIcon} alt="#" />
+              </button>
+            </div>
+          </Wrapper>
+        </div>,
+        document.body
+      )}
+    </>
+  );
+}
