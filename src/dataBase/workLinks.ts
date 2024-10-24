b61a2f085ef675cb9016cd3fc19b0536052f19cc
Agetedo <agetedo@gmail.com>
2024-10-25 01:39:52 +0300

    created array workLinks

---
+++ src/dataBase/workLinks.ts
@@ -0,0 +1,48 @@
+interface linksType {
+  id: string | number | bigint;
+  imgSrc: string;
+  imgAlt: string;
+  link: string;
+} 
+    
+export const workLinks: linksType[] = [{
+  id: "wl1",
+  imgSrc: "/workLinksImages/gallery-img-01-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl2",
+  imgSrc: "/workLinksImages/gallery-img-02-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl3",
+  imgSrc: "/workLinksImages/gallery-img-03-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl4",
+  imgSrc: "/workLinksImages/gallery-img-04-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl5",
+  imgSrc: "/workLinksImages/gallery-img-05-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl6",
+  imgSrc: "/workLinksImages/gallery-img-06-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl7",
+  imgSrc: "/workLinksImages/gallery-img-01-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+}, {
+  id: "wl8",
+  imgSrc: "/workLinksImages/gallery-img-02-tn.jpg",
+  imgAlt: "Image",
+  link: "https://developer.mozilla.org",
+},];
