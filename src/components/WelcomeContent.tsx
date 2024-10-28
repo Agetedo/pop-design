0f8af3b3e1c84098c30dc879a135dba76a1e71aa
Agetedo <agetedo@gmail.com>
2024-10-28 19:14:10 +0300

    {welcomeText}

--- src/components/WelcomeContent.tsx
+++ src/components/WelcomeContent.tsx
@@ -1,21 +1,25 @@
 import welcomeImage1 from "/welcome-1.jpg";
 import welcomeImage2 from "/welcome-2.jpg";
 import styles from "../styles/WelcomeContent.module.scss";
+const welcomeText1 = "You are allowed to download, modify and use this template for your commercial or business websites.";
+const welcomeText2 = "Please tell your friends about PopDesign. That will be very helpful for us. Thank you.";
+const welcomeText3 = "You are allowed to download, modify and use this template for your commercial or business websites.";
+const welcomeText4 = "You are allowed to download, modify and use this template for your commercial or business websites.";
 
 export default function WelcomeContent({ title }: { title: string }) {
-  return(
+  return (
     <section className={styles.content}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.contentItems}>
         <Content1 
           imgAlt1={"Image"} 
-          text1={"You are allowed to download, modify and use this template for your commercial or business websites."} 
-          text2={"Please tell your friends about PopDesign. That will be very helpful for us. Thank you."} 
+          text1={welcomeText1} 
+          text2={welcomeText2} 
         />
         <Content2 
           imgAlt2={"Image"} 
-          text3={"You are allowed to download, modify and use this template for your commercial or business websites."} 
-          text4={"You are allowed to download, modify and use this template for your commercial or business websites."} 
+          text3={welcomeText3} 
+          text4={welcomeText4} 
         />
       </div>
     </section>
@@ -28,7 +32,7 @@ interface Content1Props {
   text2: string;
 }
 function Content1({ imgAlt1, text1, text2 }: Content1Props) {
-  return(
+  return (
     <div className={styles.item}>
       <img src={welcomeImage1} alt={imgAlt1} />
       <p className={styles.text}>{text1}</p>
@@ -43,7 +47,7 @@ interface Content2Props {
   text4: string;
 }
 function Content2({ imgAlt2, text3, text4 }: Content2Props) {
-  return(
+  return (
     <div className={styles.item}>
       <p className={styles.text}>{text3}</p>
       <p className={styles.text}>{text4}</p>
