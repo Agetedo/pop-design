86e75555a37632e4c1efd77de7209d5acc1ca0b4
Agetedo <agetedo@gmail.com>
2024-10-28 19:13:34 +0300

    update to <PagePortal />

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,51 +1,10 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
-import Conteiner from "./Conteiner";
-import closeIcon from "/modal-close-icon.png";
-import ButtonPage from "./ButtonPage";
-import { buttons } from "../dataBase/buttons";
+import PagePortal from "./PagePortal";
 import WelcomeContent from "./WelcomeContent";
-import styles from "../styles/PagePortal.module.scss";
 
 export default function WelcomePage() {
-  const welcome = buttons.filter(modal =>
-    modal.buttonText === "Welcome"
-  );
-  const [showModal, setShowModal] = useState(false);
-
   return (
-    <>
-      {welcome.map(modal =>
-        <ButtonPage key={modal.id} 
-          imgSrc={modal.imgSrc} 
-          buttonText={modal.buttonText} 
-          onClick={() => setShowModal(true)} 
-        />
-      )}
-      {showModal && createPortal(
-        <ModalContent onClose={() => setShowModal(false)} />,
-        document.body
-      )}
-    </>
-  );
-}
-
-type ModalContentClose = {
-  onClose: () => void;
-};
-function ModalContent({ onClose }: ModalContentClose) {
-  return (
-    <div className={styles.page}>
-      <Conteiner className={styles.pageWrapper}>
-        <div className={styles.pageContent}>
-
-          <WelcomeContent title={"Welcome to Pop Design"} />
-          
-          <button className={styles.modalClose} onClick={onClose}>
-            <img src={closeIcon} alt="#" />
-          </button>
-        </div>
-      </Conteiner>
-    </div>
+    <PagePortal button={"Welcome"}>
+      <WelcomeContent title={"Welcome to Pop Design"} />
+    </PagePortal>
   );
 }
